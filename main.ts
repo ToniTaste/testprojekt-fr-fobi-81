e75a792fcb66e6e94e252e68104909e10bdeb5dd
Toni Taste <feedback@tinohempel.de>
2021-03-06 12:34:03 +0100

    Anpassungen und "rund machen"!

--- main.ts
+++ main.ts
@@ -4,8 +4,21 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(randint(1, 6))
+input.onButtonPressed(Button.AB, function () {
+    basic.showCompass(5000)
 })
-basic.showString("hi!")
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(0, 9))
+})
+basic.showString("HALLO!")
 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+for (let index = 0; index < 4; index++) {
+    basic.setLedColor(0xff0000)
+    basic.pause(100)
+    basic.setLedColor(0x00ff00)
+    basic.pause(100)
+    basic.setLedColor(0x007fff)
+    basic.pause(100)
+}
+basic.turnRgbLedOff()
+basic.showString("?")
